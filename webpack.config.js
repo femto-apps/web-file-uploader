0fe623e8cba13870f42a5e7fa273deaaa6c74fd4
Alexander Craggs <popey@debenclipper.com>
2019-12-05 17:48:16 +0000

    Update styling to work with the dark theme.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-    mode: 'production',
+    mode: 'development',
     entry: './src/uploader.js',
     output: {
         path: path.resolve(__dirname, 'public/js'),
