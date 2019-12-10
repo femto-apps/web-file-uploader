770c422f05ca45e89c919f9524d0125e6343216e
Alexander Craggs <popey@debenclipper.com>
2019-12-10 00:17:29 +0000

    Remove unnecessary dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: './src/uploader.js',
     output: {
         path: path.resolve(__dirname, 'public/js'),
